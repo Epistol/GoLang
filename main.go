478c3015f75346fa4b0303867ea1546256e7cc8b
Epistol <mika63270@live.fr>
2017-10-23 16:44:55 +0200

    strings

--- main.go
+++ main.go
@@ -5,28 +5,19 @@ import (
 	/*"math"
 	"strings"*/
 
+	"strings"
 )
-func Concat(integers []int){
-	integers = append(integers,1)
-	fmt.Println("In concact : ", integers)
-}
 
-func ConcatByAddr(integers *[]int){
-	*integers = append(*integers,1)
-}
-
-func ConcatbyVal(integers []int) []int{
-	return append(integers,1)
-}
 
 func main() {
 
-	slice0 := []int{-1,0}
-	Concat(slice0)
-	ConcatByAddr(&slice0)
-	fmt.Println("After : ", slice0)
-	slice0 = ConcatbyVal(slice0)
-	fmt.Println("After : ", slice0)
+	sentence := "James is such a uvavu"
+	mission := strings.Replace(sentence, "uvavu", "mamene", 1)
+	words := strings.Split(sentence, " ")
+
+	words[3] = words[3][:1]
+	sentence = strings.Join(words, " ")
+	fmt.Println( sentence )
 }
 
 // ____________________________________________________________________________
