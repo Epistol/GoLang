143d37ccf838d20ed4011a85545b110006b3cdb8
Epistol <mika63270@live.fr>
2017-10-24 09:19:36 +0200

    fichiers

---
+++ append.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	/*"math"
+	"strings"*/
+)
+
+
+-func Concat(integers []int){
+	integers = append(integers,1)
+	fmt.Println("In concact : ", integers)
+}
+
+func ConcatByAddr(integers *[]int){
+	*integers = append(*integers,1)
+}
+
+func ConcatbyVal(integers []int) []int{
+	return append(integers,1)
+}
+
+func main() {
+
+	slice0 := []int{-1,0}
+	Concat(slice0)
+	ConcatByAddr(&slice0)
+	fmt.Println("After : ", slice0)
+	slice0 = ConcatbyVal(slice0)
+	fmt.Println("After : ", slice0)
+
+}
---
+++ replalce_strings.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	/*"math"
+	"strings"*/
+
+	"strings"
+)
+
+
+func main() {
+
+	sentence := "James is such a uvavu"
+	mission := strings.Replace(sentence, "uvavu", "mamene", 1)
+	words := strings.Split(sentence, " ")
+
+	words[3] = words[3][:1]
+	sentence = strings.Join(words, " ")
+	fmt.Println(mission )
+}
